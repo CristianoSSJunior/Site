bec6ccc80f2d79bb5ad374e988bc181ffbb430fe
CristianoSSJunior <cristiano@ezvoice.com.br>
2025-08-04 14:40:19 -0300

    Change modes button inicial version finished and files changes

--- JS/JavaS.js
+++ JS/JavaS.js
@@ -1,12 +1,12 @@
 
 // Função para exibir o texto ao clicar no botão collapse
-var coll= document.getElementsByClassName("collapsible");
-var i;
+let coll= document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
+        let content = this.nextElementSibling;
         if (content.style.maxHeight){
         content.style.maxHeight = null;
         } else {
@@ -19,14 +19,19 @@ for (i = 0; i < coll.length; i++) {
         document.body.classList.toggle('darkmode');
         document.body.classList.toggle('lightmode');
         // Troca o texto do botão
-        var modeIcon = document.getElementById('mode-icon');
+        let modeIcon = document.getElementById('mode-icon');
         if (document.body.classList.contains('lightmode')) {
-            modeIcon.src = 'Assets/moonB.png'; // Ícone para modo claro
+            modeIcon.src = 'Assets/moon.png'; // Ícone para modo claro
             modeIcon.alt = 'Modo Claro';
+            document.getElementById('header').classList.add('Cabecalholightmode');
+            document.getElementById('header').classList.remove('Cabecalho');
 
         } else {
             modeIcon.src = 'Assets/solB.png'; // Ícone para modo escuro
             modeIcon.alt = 'Modo Escuro';
+            document.getElementById('header').classList.add('Cabecalho');
+            document.getElementById('header').classList.remove('Cabecalholightmode');
+            
         }
     };
 
